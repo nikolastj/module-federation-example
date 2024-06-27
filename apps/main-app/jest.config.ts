01e1f930e55bd67ab028ec208e1d97b6ed69fcd6
Nikola Stjelja <nikola.stjelja@keylight.com>
2024-06-27 12:58:18 +0200

    add main app

--- apps/main-app/jest.config.ts
+++ apps/main-app/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
-  displayName: 'init-app',
+  displayName: 'main-app',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/apps/init-app',
+  coverageDirectory: '../../coverage/apps/main-app',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
